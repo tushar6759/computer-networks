93408b953b7bf615b5d1f980ef4240b431a580fd
tushar6759 <91051637+tushar6759@users.noreply.github.com>
2021-11-30 23:08:44 +0530

    Update class_of_ip.cpp

--- class_of_ip.cpp
+++ class_of_ip.cpp
@@ -14,30 +14,40 @@ D -> 224-239
 E -> 240-255 
 */
 
-#include<iostream>
-#include<stdlib.h>
+ #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int binary[4][8] ;
-	
-	cout<<"Enter the IP in Dotted Decimal Notation:\nEnter first octet:";
-	cin>>binary[0][0];
-	cout<<"Enter second octet:";
-	cin>>binary[1][0];
-	cout<<"Enter third octet:";
-	cin>>binary[2][0];
-	cout<<"Enter fourth octet:";
-	cin>>binary[3][0];
-	
-	if( binary[0][0] >= 0 && binary[0][0] <= 127 )	
-		cout<<"It is Class A address.\n";
-	else if( binary[0][0] >= 128 && binary[0][0] <= 191 )
-		cout<<"It is Class B address.\n";	
-	else if( binary[0][0] >= 192 && binary[0][0] <= 223 )
-		cout<<"It is Class C address.\n";
-	else if( binary[0][0] >= 224 && binary[0][0] <= 2399 )
-		cout<<"It is Class D address.\n";
-	else if( binary[0][0] >= 240 && binary[0][0] <= 255 )
-		cout<<"It is Class E address.\n";
+   string s,temp;
+	cout<<"enter the ip address in dotted decimal notation: \n";
+   cin>>s;
+  int i=0;
+  while(s[i]!='.')
+  {
+    temp=temp+s[i];
+    i++;
+  }
+  int c=stoi(temp);
+  if(c>=0 && c<=127)
+  {
+     cout<<"class A";
+  }
+  else if(c>=128 && c<=191)
+  {
+     cout<<"class B";
+  }
+  else if(c>=192 && c<=223)
+  {
+     cout<<"class C";
+  }
+  else if(c>=224 && c<=239)
+  {
+     cout<<"class D";
+  }
+  else if(c>=240 && c<=255)
+  {
+     cout<<"class E";
+  }
+
+
 }
